0d61cd791c2e08b8d6b8d5056b0697c9a7e6444c
Temitope Ajisafe <ajisafet@gmail.com>
2019-08-30 17:21:48 +0100

    Exercises 3-1 to 3-3

---
+++ chap2/cars.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+cars=["BMW","Volvo","Tesla"]
+for car in cars:
+    print("I would like to own a",car+"!")
---
+++ chap2/names.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+names=["Thando","Innocent","Eric"]
+print(names[0])
+for name in names:
+    print("Hello",name+"!")
